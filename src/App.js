6613af184a0abcccc1c52b623813d4015615f7df
Aleksandr Ivanov <avonavi232@gmail.com>
2018-03-16 18:01:13 +0300

    AliensGoHome 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Canvas from './components/Canvas';
 
 import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <div></div>
-        );
+           <Canvas/>
+        ); 
     }
 }
+
+App.propTypes = {
+    message: PropTypes.string.isRequired,
+};
 
 export default App;
